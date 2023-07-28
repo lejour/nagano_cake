86e8fecc3c6708d437ff9ec28ddb2b444f4b9369
lejour <29193321+lejour@users.noreply.github.com>
2023-07-28 07:20:40 +0000

    admin item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,24 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to action: :show
+  end
 
   def index
+    @items = Item.page(params[:page])
 
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
   end
 
   private
